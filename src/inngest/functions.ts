71ca0d023bf98fddd2dee2b00a2c32bf49565e08
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2025-06-29 11:28:07 +0530

    ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { gemini, openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert Next.js developer. You write readable and maintainable code. You write simple next.js & react snippets.",
+      model: gemini({ model: "gemini-1.5-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following code Snippet: ${event.data.value}`
+    );
+    
+    return { output }
   },
 );
